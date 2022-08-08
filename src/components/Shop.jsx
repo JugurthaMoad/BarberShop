4de62410622d586c36f37465fc4de1454b7ee18a
MJ <moadjugurrtha@gmail.com>
2022-08-08 17:27:34 +0200

    add persist on refresh with LocalStorage to cart elements

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,16 @@
 import Nav from "./shopping/nav";
 import { Outlet } from "react-router-dom";
 import CartContext from "../context/CartContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Shop = () => {
-  const [listArticles, setList] = useState([]);
-  const [CartArticles, setCartArticles] = useState(0);
+  const [listArticles, setList] = useState(() => {
+    const data = localStorage.getItem("listArticles");
+    return data ? JSON.parse(data) : [];
+  });
+  const [CartArticles, setCartArticles] = useState(() => {
+    const data = localStorage.getItem("CartArticles");
+    return data ? JSON.parse(data) : 0;
+  });
 
   // this function will add elements in cart
   const addElement = (el) => {
@@ -35,7 +41,21 @@ const Shop = () => {
       setCartArticles(CartArticles - 1);
     }
   };
+  useEffect(() => {
+    let data1 = localStorage.getItem("listArticles");
+    let data2 = localStorage.getItem("CartArticles");
 
+    if (data1) {
+      setList(JSON.parse(data1));
+    }
+    if (data2) {
+      setCartArticles(JSON.parse(data2));
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("listArticles", JSON.stringify(listArticles));
+    localStorage.setItem("CartArticles", JSON.stringify(CartArticles));
+  }, [listArticles, CartArticles]);
   return (
     <div>
       <CartContext.Provider
